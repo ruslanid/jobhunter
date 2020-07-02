4ab0e4fb8ad4712f72079d4519061f35959188a9
unknown <ruslan.idrisov01@gmail.com>
2020-07-02 12:05:59 -0400

    Added user profile page

--- react-client/src/pages/profile/profile.page.jsx
+++ react-client/src/pages/profile/profile.page.jsx
@@ -1,9 +1,75 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
 
-import {} from './profile.styles';
+import {
+  ProfileHeaderContainer,
+  UpdateProfileFormContainer,
+  DeleteButtonContainer
+} from './profile.styles';
 
-const ProfilePage = () => (
-  <div className="profile-page">Profile page</div>
-);
+import BackToLink from '../../components/back-to-link/back-to-link.component';
+import FormInput from '../../components/form-input/form-input.component';
+import CustomButton from '../../components/custom-button/custom-button.component';
 
-export default ProfilePage;
+import { selectCurrentUser } from '../../redux/users/users.selectors';
+
+const ProfilePage = ({currentUser}) => {
+
+  const {id, firstName, lastName, username} = currentUser;
+
+  const [userDetails, setUserDetails] = useState({
+    id, firstName, lastName, username
+  });
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const editedUser = {...userDetails};
+  };
+
+  const handleChange = event => {
+    const {name, value} = event.target;
+    setUserDetails({...userDetails, [name]: value});
+  };
+  
+  return (
+    <div className="profile-page">
+      <BackToLink to="/jobs" destination="Dashboard" />
+      <UpdateProfileFormContainer onSubmit={handleSubmit}>
+        <ProfileHeaderContainer>{firstName}'s Profile</ProfileHeaderContainer>
+        <FormInput
+          type="text"
+          name="firstName"
+          placeholder="First Name"
+          value={userDetails.firstName}
+          handleChange={handleChange}
+        />
+        <FormInput
+          type="text"
+          name="lastName"
+          placeholder="Last Name"
+          value={userDetails.lastName}
+          handleChange={handleChange}
+        />
+        <FormInput
+          type="text"
+          name="username"
+          placeholder="Email"
+          value={userDetails.username}
+          handleChange={handleChange}
+        />
+        <CustomButton type="submit">Save Profile</CustomButton>
+      </UpdateProfileFormContainer>
+      <DeleteButtonContainer>
+        <CustomButton type="submit" removeButton>Remove All Jobs</CustomButton>
+        <CustomButton type="submit" removeButton>Delete My Profile</CustomButton>
+      </DeleteButtonContainer>
+    </div>
+  )
+};
+
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
+});
+
+export default connect(mapStateToProps)(ProfilePage);
---
+++ react-client/src/pages/profile/profile.styles.jsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const UpdateProfileFormContainer = styled.form`
+  width: 30%;
+  margin: 40px auto 0;
+
+  @media (max-width: 1200px) {
+    width: 40%;
+  }
+
+  @media (max-width: 800px) {
+    width: 80%;
+  }
+`;
+
+export const ProfileHeaderContainer = styled.h1`
+  font-weight: 500;
+  text-align: center;
+  margin: 40px 0 40px;
+  color: gray;
+
+  @media (max-width: 800px) {
+    width: 80%;
+    margin: 40px auto 60px;
+    font-size: 24px;
+  }
+`;
+
+export const DeleteButtonContainer = styled.div`
+  width: 30%;
+  margin: 0px auto 60px;
+
+  @media (max-width: 1200px) {
+    width: 40%;
+  }
+
+  @media (max-width: 800px) {
+    width: 80%;
+  }
+`;
